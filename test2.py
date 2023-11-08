7bdd8ea0c2417f47b65bb9240f12cbe157d08235
jonbvc2023 <144270657+jonbvc2023@users.noreply.github.com>
2023-11-08 11:39:54 -0700

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,7 @@
+num1 = 10
+
+num2 = 10
+
+result = num1+num2
+
+print(result)
---
+++ test3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number: "))
+
+num2 = int(input("Enter a another a number: "))
+
+result = num1+num2
+
+print(result);
